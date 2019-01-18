2070cb84449a4c7d9caae976d3a64678bc49bc61
Paulina Papiernia <p_papiernia@wp.pl>
2019-01-18 20:23:17 +0100

    poprawiona wersja

--- zadanie-ajax/js/main.js
+++ zadanie-ajax/js/main.js
@@ -1,13 +1,14 @@
-function afterDataReceived(data) {
-    let divElement = $('<div></div>');
-    divElement.html(data);
-    divElement.appendTo('body');
-  }
-  
-  function getData() {
-    $.get('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', afterDataReceived);
-  }
-  
-  $(document).ready(function() {
-    $('button').click(getData);
-  });
+$(function () {
+    $('#przycisk').click(function () {
+        $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function (data) {
+            $("div").append
+            $('#dane-programisty').html('<p> Imię: ' + data.imie + '</p>');
+            $('#dane-programisty').append('<p> Nazwisko: ' + data.nazwisko + '</p>');
+            $('#dane-programisty').append('<p> Zawód: ' + data.zawod + '</p>');
+            $('#dane-programisty').append('<p> Firma: ' + data.firma + '</p>')
+        });
+    })
+})
+let $newdiv1 = $("<div id='dane-programisty'></div>");
+ 
+$("button").after($newdiv1);
